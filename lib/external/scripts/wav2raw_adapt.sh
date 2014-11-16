a70c75d4d6e3a118182d69efe72dddd6660b8d7e
sjai013 <sjai013.ece@gmail.com>
2014-11-17 09:00:00 +1300

    files deleted

--- lib/external/scripts/wav2raw_adapt.sh
+++
@@ -1,9 +0,0 @@
-for f in `find $1/ -name *.wav | sed "s|$1/||g"`; do
-	dir=$(dirname $f)
-	filename=$(basename "$f")
-	echo $filename
-	mkdir -p $2/$dir
-	sox $1/$f $2/$dir/${filename%.*}.raw
-done
-
-
